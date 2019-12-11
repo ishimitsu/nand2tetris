1b3c2b7c4e2ad6ba854efdb6f071464ccb5394dc
Mitsutaka Shiraishi <srismttk@gmail.com>
2019-12-12 00:43:53 +0900

    implement all Parser modules in Asm_Parser.py

--- projects/06/hack_assembler/Asm_Parser.py
+++ projects/06/hack_assembler/Asm_Parser.py
@@ -2,35 +2,75 @@ import os
 import sys
 
 class Parser:
-    A_COMMAND = 0
-    C_COMMAND = 1
-    L_COMMAND = 2  
-    
     def __init__(self, asm_file):
-        self.asm_file = asm_file
-
-    def hasMoreCommands():
+        self.cur_line   = 0
+        self.cur_cmd    = ""
+        self.cmd_type   = "NOT_COMMAND"
+        self.a_symbol = ""
+        self.c_dest = self.c_comp = self.c_jump = ""
+        
+        if os.path.isfile(asm_file):
+            fp = open(asm_file)
+            self.readlines = fp.readlines()
+            self.lines = len(self.readlines)
+            fp.close()
+        
+    def hasMoreCommands(self):
         ret = False
+        if self.cur_line < self.lines:
+            ret = True
         return ret
 
-    def advance():
+    def advance(self):
+        self.cur_cmd = self.readlines[self.cur_line]
+        self.cur_line+=1
         return
 
-    def commandType():
-        ret = A_COMMAND
-        return ret
+    def commandType(self):
+        self.cmd_type = "NOT_COMMAND"
+        self.a_symbol = ""
+        self.c_dest = self.c_comp = self.c_jump = ""
+        
+        cmd = self.cur_cmd.split()
+        if( len(cmd) == 0):
+            self.cmd_type = "NOT_COMMAND"            
+            return "NOT_COMMAND"
 
-    def symbol():
-        ret = ""
-        return ret
+        cmd_head = cmd[0]
+        if   cmd_head.startswith('//'):
+            self.cmd_type = "NOT_COMMAND"
+        elif cmd_head.startswith('@'):
+            self.cmd_type = "A_COMMAND"
+            self.a_symbol   = cmd_head.strip('@')
+        elif cmd_head.startswith('('):
+            self.cmd_type = "L_COMMAND"
+            self.a_symbol   = cmd_head.strip('()')
+        elif '=' in cmd_head:
+            self.cmd_type = "C_COMMAND"
+            comp_dest = cmd_head.split('=')
+            self.c_dest = comp_dest[0]
+            self.c_comp = comp_dest[1]            
+        elif ';' in cmd_head:
+            self.cmd_type = "C_COMMAND"        
+            comp_jump = cmd_head.split(';')
+            self.c_comp = comp_jump[0]
+            self.c_jump = comp_jump[1]            
+        else :
+            self.cmd_type = "NOT_COMMAND"
 
-    def dest():
-        ret = ""
-        return ret
+        return self.cmd_type
+
+    def symbol(self):
+        return self.a_symbol
+
+    def dest(self):
+        return self.c_dest
+
+    def comp(self):
+        return self.c_comp
     
-    def jump():
-        ret = ""
-        return ret
+    def jump(self):
+        return self.c_jump
         
 
         
--- projects/06/hack_assembler/Hack_Assembler.py
+++ projects/06/hack_assembler/Hack_Assembler.py
@@ -5,22 +5,33 @@ from Asm_Parser import Parser
 from Asm_Code import Code
 from Asm_SymbolTable import SymbolTable
 
-def asm_parser(asm_file):
-    if os.path.isfile(asm_file):
-        fp = open(asm_file)
-        readline = fp.readlines()
-        print("line ", len(readline))
-        for i in range(0, len(readline)):
-            print(readline[i])
+def test_Parser(parser):
+    ret = True
 
-        fp.close()
+    while parser.hasMoreCommands():
+        parser.advance()
+        cmd_type = parser.commandType()
+
+        if cmd_type == "A_COMMAND" or cmd_type == "L_COMMAND":
+            symbol = parser.symbol()            
+            print("TYPE:", cmd_type, "\t Symbol:", symbol)            
+        elif cmd_type == "C_COMMAND":
+            dest = parser.dest()
+            comp = parser.comp()
+            jump = parser.jump()            
+            print("TYPE:", cmd_type, "\t dest:", dest, "\t comp:", comp, "\t jump:", jump)
+            
+    return ret
 
 if __name__ == '__main__':
 
     args = sys.argv
     if len(args) == 2:
         parser = Parser(args[1])
-        asm_parser(parser.asm_file);
+        symbol_table = SymbolTable();
+
+        test_Parser(parser)
+                
     else:
         print("Invalid Args!");
         
