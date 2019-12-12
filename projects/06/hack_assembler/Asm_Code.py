c1c1bb26d622134b56846c9924534e5ec91454a3
Mitsutaka Shiraishi <srismttk@gmail.com>
2019-12-13 01:34:03 +0900

    finished to implement Code class, now Hack_Assembler can convert C_COMMAND to binary code

--- projects/06/hack_assembler/Asm_Code.py
+++ projects/06/hack_assembler/Asm_Code.py
@@ -2,18 +2,73 @@ import os
 import sys
 
 class Code:
+
+    # 3bit bincode
+    dest_dic = {
+        ""    : 0b000,
+        "M"   : 0b001,
+        "D"   : 0b010,
+        "MD"  : 0b011,
+        "A"   : 0b100,
+        "AM"  : 0b101,
+        "AD"  : 0b110,
+        "AMD" : 0b111
+    }
     
-    def dest(mnemonic):
-        ret = 0 # 3bit
-        return ret
+    # 7bit bincode    
+    comp_dic = {
+        # case a=0, comp=0xxxxxx
+        "0"  : 0b0101010,
+        "1"  : 0b0111111,
+        "-1" : 0b0111010,
+        "D"  : 0b0001100,
+        "A"  : 0b0110000,
+        "!D" : 0b0001101,
+        "!A" : 0b0110001,
+        "-D" : 0b0001111,
+        "-A" : 0b0110011,
+        "D+1": 0b0011111,
+        "A+1": 0b0110111,
+        "D-1": 0b0001110,
+        "A-1": 0b0110010,
+        "D+A": 0b0000010,
+        "D-A": 0b0010011,
+        "A-D": 0b0000111,
+        "D&A": 0b0000000,
+        "D|A": 0b0010101,
+        # case a=1, comp=1xxxxxx
+        "M"  : 0b1110000,
+        "!M" : 0b1110001,
+        "-M" : 0b1110011,
+        "M+1": 0b1110111,
+        "M-1": 0b1110010,
+        "D+M": 0b1000010,
+        "D-M": 0b1010011,
+        "M-D": 0b1000111,
+        "D&M": 0b1000000,
+        "D|M": 0b1010101
+    }
 
-    def comp(mnemonic):
-        ret = 0 # 7bit
-        return ret
+    # 3bit bincode    
+    jump_dic = {
+        ""    : 0b000,
+        "JGT" : 0b001,
+        "JEQ" : 0b010,
+        "JGE" : 0b011,
+        "JLT" : 0b100,
+        "JNE" : 0b101,
+        "JLE" : 0b110,
+        "JMP" : 0b111
+    }
+    
+    def dest(self, mnemonic):
+        return self.dest_dic.setdefault(mnemonic, "")
 
-    def jump(mnemonic):
-        ret = 0 # 3bit
-        return ret
+    def comp(self, mnemonic):
+        return self.comp_dic.setdefault(mnemonic, "0")        
+
+    def jump(self, mnemonic):
+        return self.jump_dic.setdefault(mnemonic, "")                
     
         
              
--- projects/06/hack_assembler/Hack_Assembler.py
+++ projects/06/hack_assembler/Hack_Assembler.py
@@ -5,33 +5,53 @@ from Asm_Parser import Parser
 from Asm_Code import Code
 from Asm_SymbolTable import SymbolTable
 
-def test_Parser(parser):
-    ret = True
-
+def convert_asm_to_bincode(asm_file):
+    parser = Parser(asm_file)
+    code   = Code()
+    symbol_table = SymbolTable();    
+    
     while parser.hasMoreCommands():
         parser.advance()
         cmd_type = parser.commandType()
 
-        if cmd_type == "A_COMMAND" or cmd_type == "L_COMMAND":
-            symbol = parser.symbol()            
-            print("TYPE:", cmd_type, "\t Symbol:", symbol)            
+        if cmd_type in {"A_COMMAND", "L_COMMAND"}:
+            print(parser.cur_cmd, end="=> ")  
+            a_symbol = parser.symbol()            
+            print("TYPE:", cmd_type, "\t Symbol:", a_symbol, end="\t\t=>\t")
+            print("BINCODE:")            
         elif cmd_type == "C_COMMAND":
-            dest = parser.dest()
-            comp = parser.comp()
-            jump = parser.jump()            
-            print("TYPE:", cmd_type, "\t dest:", dest, "\t comp:", comp, "\t jump:", jump)
-            
-    return ret
+            print(parser.cur_cmd, end="=> ")              
+            c_dest = parser.dest()
+            c_comp = parser.comp()
+            c_jump = parser.jump()
+
+            c_cmd_code = 0b111 #3bit
+            comp_code  = 0b0   #7it
+            dest_code  = 0b000 #3bit
+            jump_code  = 0b000 #3bit
+            bin_code   = 0b0   #16bit
+            if   len(c_dest) > 0:
+                comp_code  = code.comp(c_comp)
+                dest_code  = code.dest(c_dest)                
+                jump_code  = 0b000
+                print("TYPE:", cmd_type, "\t dest:", c_dest, "\t comp:", c_comp, end="\t=>\t")   
+                
+            elif len(c_jump) > 0:
+                comp_code = code.comp(c_comp)
+                dest_code = 0b000                
+                jump_code = code.jump(c_jump)
+                print("TYPE:", cmd_type, "\t comp:", c_comp, "\t jump:", c_jump, end="\t=>\t")
+
+            bin_code = bin(c_cmd_code<<13 | comp_code<<6 | dest_code<<3 | jump_code)
+            print("BINCODE:", bin_code)   
+    return
 
 if __name__ == '__main__':
 
     args = sys.argv
     if len(args) == 2:
-        parser = Parser(args[1])
-        symbol_table = SymbolTable();
-
-        test_Parser(parser)
-                
+        asm_file   = args[1]
+        parsed_cmd = convert_asm_to_bincode(asm_file)
     else:
         print("Invalid Args!");
         
