7db7bf367786304ee00853d387926e554595c304
Mitsutaka Shiraishi <srismttk@gmail.com>
2019-12-15 01:37:05 +0900

    move part of 'cvt c_cmd to bincode' at get_c_cmd_bincode()

--- projects/06/hack_assembler/Hack_Assembler.py
+++ projects/06/hack_assembler/Hack_Assembler.py
@@ -5,7 +5,29 @@ from Asm_Parser import Parser
 from Asm_Code import Code
 from Asm_SymbolTable import SymbolTable
 
-heap_mem_start_addr = 0b0010
+def get_c_cmd_bincode (parser, code):
+    c_dest = parser.dest()
+    c_comp = parser.comp()
+    c_jump = parser.jump()
+
+    c_cmd_code = 0b111 #3bit
+    comp_code  = 0b0   #7it
+    dest_code  = 0b000 #3bit
+    jump_code  = 0b000 #3bit
+    
+    if   len(c_dest) > 0:
+        comp_code  = code.comp(c_comp)
+        dest_code  = code.dest(c_dest)                
+        jump_code  = 0b000
+    elif len(c_jump) > 0:
+        comp_code = code.comp(c_comp)
+        dest_code = 0b000                
+        jump_code = code.jump(c_jump)
+
+    bincode = bin(c_cmd_code<<13 | comp_code<<6 | dest_code<<3 | jump_code)[2:]
+    print("TYPE:", parser.commandType(), " => BINCODE:", bincode)                    
+    return bincode
+
 
 def convert_asm_to_bincode(asm_file, hack_file):
     parser = Parser(asm_file)
@@ -16,40 +38,21 @@ def convert_asm_to_bincode(asm_file, hack_file):
     while parser.hasMoreCommands():
         parser.advance()
         cmd_type = parser.commandType()
-        bin_code = 0b0   #16bit
+        bincode = 0b0   #16bit
         # print(parser.cur_cmd, end="=> ")  
         
         if cmd_type in {"A_COMMAND", "L_COMMAND"}:
             a_symbol = parser.symbol()
-
             if a_symbol.isdigit():
                 a_value = int(a_symbol)
-                bin_code = format(a_value, '016b')
+                bincode = format(a_value, '016b')
                 
-            bincode_list.append(bin_code)
-            print("TYPE:", cmd_type, " => BINCODE:", bin_code) 
+            bincode_list.append(bincode)
+            print("TYPE:", cmd_type, " => BINCODE:", bincode) 
 
         elif cmd_type == "C_COMMAND":
-            c_dest = parser.dest()
-            c_comp = parser.comp()
-            c_jump = parser.jump()
-
-            c_cmd_code = 0b111 #3bit
-            comp_code  = 0b0   #7it
-            dest_code  = 0b000 #3bit
-            jump_code  = 0b000 #3bit
-            if   len(c_dest) > 0:
-                comp_code  = code.comp(c_comp)
-                dest_code  = code.dest(c_dest)                
-                jump_code  = 0b000
-            elif len(c_jump) > 0:
-                comp_code = code.comp(c_comp)
-                dest_code = 0b000                
-                jump_code = code.jump(c_jump)
-
-            bin_code = bin(c_cmd_code<<13 | comp_code<<6 | dest_code<<3 | jump_code)[2:]
-            bincode_list.append(bin_code)
-            print("TYPE:", cmd_type, " => BINCODE:", bin_code)            
+            bincode = get_c_cmd_bincode(parser, code)
+            bincode_list.append(bincode)
 
     with open(hack_file, mode='w') as fp_hack:
         fp_hack.writelines('\n'.join(bincode_list))
