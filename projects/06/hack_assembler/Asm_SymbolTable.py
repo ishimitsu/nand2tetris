ab345de4f7ab33cb0b7537b713a4abcdaaf57cc8
Mitsutaka Shiraishi <srismttk@gmail.com>
2019-12-15 20:29:10 +0900

    store l_cmd_symbol to SymbolTable with RomAddr

--- projects/06/hack_assembler/Asm_SymbolTable.py
+++ projects/06/hack_assembler/Asm_SymbolTable.py
@@ -2,21 +2,32 @@ import os
 import sys
 
 class SymbolTable:
+
+    val_symbol_start_addr = 0x0010
+    symbol_table = {
+        "SP"     : 0x0, "LCL"    : 0x1, "ARG"    : 0x2, "THIS"   : 0x3, "THAT"   : 0x4,
+        "R0"     : 0x0, "R1"     : 0x1, "R2"     : 0x2, "R3"     : 0x3,
+        "R4"     : 0x4, "R5"     : 0x5, "R6"     : 0x6, "R7"     : 0x7,
+        "R8"     : 0x8, "R9"     : 0x9, "R10"    : 0xa, "R11"    : 0xb,
+        "R12"    : 0xc, "R13"    : 0xd, "R14"    : 0xe, "R15"    : 0xf,              
+        "SCREEN" : 0x4000, "KBD"    : 0x6000
+    }
     
     def __init__(self):
-        self.symbol_table = []
+        self.val_symbol_table = []
 
     def addEntry(self, symbol, address):
-        self.symbol_table.extend([symbol, address])
+        # new = {symbol, address}
+        # print("new l label", new)
+        # self.symbol_table.update(new)
+        self.symbol_table[symbol] = address
         return
 
     def contains(self, symbol):
-        ret = False
-        return ret
+        return symbol in self.symbol_table
 
     def getAddress(self, symbol):
-        ret = 0x0;
-        return ret
+        return self.symbol_table.get(symbol)
         
     
 
--- projects/06/hack_assembler/Hack_Assembler.py
+++ projects/06/hack_assembler/Hack_Assembler.py
@@ -1,64 +1,108 @@
 import os
 import sys
+import pprint
 
 from Asm_Parser import Parser 
 from Asm_Code import Code
 from Asm_SymbolTable import SymbolTable
 
-def get_c_cmd_bincode (parser, code):
-    c_dest = parser.dest()
-    c_comp = parser.comp()
-    c_jump = parser.jump()
-
-    c_cmd_code = 0b111 #3bit
-    comp_code  = 0b0   #7it
-    dest_code  = 0b000 #3bit
-    jump_code  = 0b000 #3bit
+class HackAssember:
     
-    if   len(c_dest) > 0:
-        comp_code  = code.comp(c_comp)
-        dest_code  = code.dest(c_dest)                
-        jump_code  = 0b000
-    elif len(c_jump) > 0:
-        comp_code = code.comp(c_comp)
-        dest_code = 0b000                
-        jump_code = code.jump(c_jump)
-
-    bincode = bin(c_cmd_code<<13 | comp_code<<6 | dest_code<<3 | jump_code)[2:]
-    print("TYPE:", parser.commandType(), " => BINCODE:", bincode)                    
-    return bincode
-
-
-def convert_asm_to_bincode(asm_file, hack_file):
-    parser = Parser(asm_file)
-    code   = Code()
-    symbol_table = SymbolTable();
-    bincode_list = []
+    def __init__(self):
+        self.cur_rom_addr = 0x0
+        self.bincode_romaddr_table = {}
     
-    while parser.hasMoreCommands():
-        parser.advance()
-        cmd_type = parser.commandType()
-        bincode = 0b0   #16bit
-        # print(parser.cur_cmd, end="=> ")  
+    def add_new_bincode(self, bincode):
+        # new = {self.cur_rom_addr, bincode}
+        # self.bincode_romaddr_table.update(new)
+        # self.cur_rom_addr+=0x1
+        return
+    
+    def get_a_cmd_bincode (self,  parser, code, symbol_table):
+        a_symbol = parser.symbol()
+        bincode = 0b0
         
-        if cmd_type in {"A_COMMAND", "L_COMMAND"}:
-            a_symbol = parser.symbol()
-            if a_symbol.isdigit():
-                a_value = int(a_symbol)
-                bincode = format(a_value, '016b')
-                
-            bincode_list.append(bincode)
-            print("TYPE:", cmd_type, " => BINCODE:", bincode) 
-
-        elif cmd_type == "C_COMMAND":
-            bincode = get_c_cmd_bincode(parser, code)
-            bincode_list.append(bincode)
-
-    with open(hack_file, mode='w') as fp_hack:
-        fp_hack.writelines('\n'.join(bincode_list))
-        fp_hack.write('\n')
+        if a_symbol.isdigit():
+            a_value = int(a_symbol)
+            bincode = format(a_value, '016b')
+        else:
+            # dummy
+            a_value = 0b0
+            bincode = format(a_value, '016b')            
             
-    return
+        # print("TYPE:", parser.commandType(), " => BINCODE:", bincode)                            
+        return bincode
+
+    def get_c_cmd_bincode (self, parser, code):
+        c_dest = parser.dest()
+        c_comp = parser.comp()
+        c_jump = parser.jump()
+
+        c_cmd_code = 0b111 #3bit
+        comp_code  = 0b0   #7it
+        dest_code  = 0b000 #3bit
+        jump_code  = 0b000 #3bit
+    
+        if   len(c_dest) > 0:
+            comp_code  = code.comp(c_comp)
+            dest_code  = code.dest(c_dest)                
+            jump_code  = 0b000
+        elif len(c_jump) > 0:
+            comp_code = code.comp(c_comp)
+            dest_code = 0b000                
+            jump_code = code.jump(c_jump)
+
+        bincode = bin(c_cmd_code<<13 | comp_code<<6 | dest_code<<3 | jump_code)[2:]
+        # print("TYPE:", parser.commandType(), " => BINCODE:", bincode)
+            
+        return bincode
+
+
+    def cvt_a_c_cmd2bincode(self, asm_file, symbol_table):
+        parser = Parser(asm_file)
+        code   = Code()
+        bincode_list = []
+        while parser.hasMoreCommands():
+            parser.advance()
+            cmd_type = parser.commandType()
+            if cmd_type == "A_COMMAND":
+                bincode = self.get_a_cmd_bincode(parser, code, symbol_table)
+                bincode_list.append(bincode)
+                self.add_new_bincode(bincode)
+            elif cmd_type == "C_COMMAND":
+                bincode = self.get_c_cmd_bincode(parser, code)
+                bincode_list.append(bincode)
+                self.add_new_bincode(bincode)
+        return bincode_list
+
+    
+    def find_l_cmd_symbol2table(self, asm_file, symbol_table):
+        parser = Parser(asm_file)
+        rom_addr = 0x0
+        while parser.hasMoreCommands():
+            parser.advance()
+            cmd_type = parser.commandType()
+            if cmd_type == "L_COMMAND":
+                symbol = parser.symbol()                
+                symbol_table.addEntry(symbol, rom_addr+1)
+            elif cmd_type in {"A_COMMAND", "C_COMMAND"}:
+                rom_addr+=1
+        return
+    
+    
+    def convert_asm2bincode(self, asm_file, hack_file):
+        symbol_table = SymbolTable();
+
+        self.find_l_cmd_symbol2table(asm_file, symbol_table)
+        pprint.pprint(symbol_table.symbol_table)
+        bincode_list = self.cvt_a_c_cmd2bincode(asm_file, symbol_table)
+        pprint.pprint(bincode_list)        
+        
+        with open(hack_file, mode='w') as fp_hack:
+            fp_hack.writelines('\n'.join(bincode_list))
+            fp_hack.write('\n')
+
+        return
 
 if __name__ == '__main__':
 
@@ -66,8 +110,8 @@ if __name__ == '__main__':
     if len(args) == 2:
         asm_file   = args[1]
         hack_file  = os.path.split(asm_file)[0] + '/' + os.path.basename(asm_file).split('.', 1)[0] + ".hack"
-        print(hack_file)
-        parsed_cmd = convert_asm_to_bincode(asm_file, hack_file)
+        hack_assembler = HackAssember()        
+        parsed_cmd     = hack_assembler.convert_asm2bincode(asm_file, hack_file)
     else:
         print("Invalid Args!");
         
